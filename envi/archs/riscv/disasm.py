8bbe50f933e979c031d168bd7734c00a3ce755fa
Thomas <thomas.somerv@gmail.com>
2021-10-21 14:44:15 -0400

    split out operands

--- envi/archs/riscv/disasm.py
+++ envi/archs/riscv/disasm.py
@@ -1,73 +1,7 @@
-class RiscVOpcode():
-    def __init__(self):
-        pass
-
-class RiscVRegOperand():
-    def __init__(self):
-        pass
-
-    def setOperValue(self):
-        pass
-
-    def getOperValue(self):
-        pass
-
-    def getOperAddr(self):
-        pass
-
-class RiscVImmOperand():
-    def __init__(self):
-        pass
-
-    def setOperValue(self):
-        pass
-
-    def getOperValue(self):
-        pass
-
-    def getOperAddr(self):
-        pass
-
-class RiscVCRegOperand():
-    def __init__(self):
-        pass
-
-    def setOperValue(self):
-        pass
-
-    def getOperValue(self):
-        pass
-
-    def getOperAddr(self):
-        pass
-
-class RiscVRMOperand():
-    def __init__(self):
-        pass
-
-    def setOperValue(self):
-        pass
-
-    def getOperValue(self):
-        pass
-
-    def getOperAddr(self):
-        pass
-
 class RiscVDisasm():
     def __init__(self):
         pass
 
     def disasm(self, bytez, offset=0, va=0):
-        pass
-
-
-
-#must come after all operands
-class RiscVOpcode():
-    def __init__(self):
-        pass
-
-    def getBranches(self):
         pass
 
---
+++ envi/archs/riscv/operands.py
@@ -0,0 +1,56 @@
+class RiscVOpcode():
+    def __init__(self):
+        pass
+
+class RiscVRegOperand():
+    def __init__(self):
+        pass
+
+    def setOperValue(self):
+        pass
+
+    def getOperValue(self):
+        pass
+
+    def getOperAddr(self):
+        pass
+
+class RiscVImmOperand():
+    def __init__(self):
+        pass
+
+    def setOperValue(self):
+        pass
+
+    def getOperValue(self):
+        pass
+
+    def getOperAddr(self):
+        pass
+
+class RiscVCRegOperand():
+    def __init__(self):
+        pass
+
+    def setOperValue(self):
+        pass
+
+    def getOperValue(self):
+        pass
+
+    def getOperAddr(self):
+        pass
+
+class RiscVRMOperand():
+    def __init__(self):
+        pass
+
+    def setOperValue(self):
+        pass
+
+    def getOperValue(self):
+        pass
+
+    def getOperAddr(self):
+        pass
+
